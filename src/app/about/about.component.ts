38b1eae8c7a1cae3ef97a33fbfd0beed4ea327c0
foxbeckya <foxbeckya@gmail.com>
2019-08-13 13:25:21 -0400

    about page updates

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  about='this is a page about the garden store'
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
